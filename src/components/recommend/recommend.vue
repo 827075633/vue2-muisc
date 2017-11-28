297629c7e528b6d1f4bfddb50c77e943da10aea4
mengyan <827075633@qq.com>
2017-11-28 23:03:01 +0800

    轮播图初步实现，dom.js增加

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,34 +1,50 @@
 <template>
   <div class="recommend" ref="recommend">
-      <div>
-        <div class="slider-wrapper">
-        </div>
-        <div class="recommend-list">
-          <h1 class="list-title">热门歌单推荐</h1>
-          <ul>
-          </ul>
-        </div>
+    <div>
+      <div class="slider-wrapper"  v-if="recommends.length">
+        <slider>
+          <div v-for="item in recommends" key>
+            <a :href="item.linkUrl">
+              <img :src="item.picUrl" alt="">
+            </a>
+          </div>
+        </slider>
       </div>
+      <div class="recommend-list">
+        <h1 class="list-title">热门歌单推荐</h1>
+        <ul>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 <script type='text/ecmascript-6'>
-import {getRecommend} from 'api/recommend'
-import {ERR_OK} from 'api/config'
-export default {
-  created() {
-    this._getRecommend()
-  },
-  methods: {
-    _getRecommend() {
-      getRecommend().then((res) => {
-        if (res.code === ERR_OK) {
-          this.recommends = res.data.slider
-          console.log(this.recommends)
-        }
-      })
+  import Slider from 'base/slider/slider'
+  import {getRecommend} from 'api/recommend' // 引用封装好的js
+  import {ERR_OK} from 'api/config'// 引用封装好的js
+  export default {
+    data() {
+      return {
+        recommends: []
+      }
+    },
+    components: {
+      Slider
+    },
+    created() {
+      this._getRecommend()
+    },
+    methods: {
+      _getRecommend() {
+        getRecommend().then((res) => {
+          if (res.code === ERR_OK) {
+            this.recommends = res.data.slider
+            console.log(this.recommends)
+          }
+        })
+      }
     }
   }
-}
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
-/* Vue.config.productionTip = false */
 import 'common/stylus/index.styl'
-/* eslint-disable no-new */
+Vue.config.productionTip = false
+/* eslint-disable no-unused-vars */
 
 fastclick.attach(document.body)
+
+/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
